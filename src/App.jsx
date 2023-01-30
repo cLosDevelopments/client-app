fc2cc5a558b3a0bc5c25e7adf66e636c1eb4ef3d
CarlosNieto92 <114544760+cLosDevelopments@users.noreply.github.com>
2023-01-30 07:04:26 -0800

    App Creation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import FormTask from './Components/FormTask/FormTask';
+import Footer from './Components/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Client Schedualer</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/FormTask/FormTask.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+export const FormTask = (props) => {
+  return (
+    <div>FormTask</div>
+  )
+}
+
+const mapStateToProps = (state) => ({})
+
+const mapDispatchToProps = {}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FormTask)
